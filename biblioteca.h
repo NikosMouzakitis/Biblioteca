3483fd35f712f119413bf46fc39ebd95d0b4dfab
NikosMouzakitis <mzktsn@gmail.com>
2016-03-16 07:03:44 +0200

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -3,6 +3,46 @@
 
 #include <math.h>
 
+
+void print_binary(int num){
+   
+    int i,n,rm;
+    i = num;
+
+    if(i == 0)
+        printf("0");
+
+    else if ( i == 1)
+            printf("1");
+    else
+    {
+        for( n = 0; pow(2.0,n) <= i; n++)
+                        ;
+
+        n--;
+
+        while(n >= 0){
+
+            if( i >= pow(2.0,n))
+            {
+                printf("1");
+                i = i % (int ) pow(2.0,n);
+                n--;
+            }
+            else if (i < pow(2.0,n))
+            {
+
+                printf("0");
+                n--;
+            }
+
+        }
+
+    }
+
+}
+
+
 int isPrime(int n) // if n prime returns value of n,else returns zero(0)
 {
    int i, count = 0;
