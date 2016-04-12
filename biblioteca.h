4ad5d1a0b2ca282ceae8f643dc5d3ec0431355e3
NikosMouzakitis <mzktsn@gmail.com>
2016-04-13 02:17:15 +0300

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -4,8 +4,63 @@
 #include <math.h>
 
 
-void print_binary(int num){
-   
+// returns a pointer (integer ) to the space allocated for the matrice.
+
+int * create_matrice(int columns,int rows)
+{
+    int *mat;
+    mat = malloc( columns *rows *sizeof(int));
+
+
+    return mat;
+}
+
+
+// arguments is the pointer to the matrice,columns and rows.prints the output to the screen.
+
+void print_matrice(int *mat,int columns,int rows)
+{
+    int i,j;
+    printf("Printing the matrice: \n");
+
+    for(i=0; i<columns; i++)
+    {
+        for(j=0; j<rows; j++)
+        {
+            printf("%d\t",*(mat+i+j));
+        }
+        printf("\n");
+    }
+
+}
+
+// initializes the matrice with random numbers.
+
+void random_init(int *mat,int columns,int rows)
+{
+    int i,j;
+
+    for(i=0; i<columns; i++)
+
+        for(j=0; j<rows; j++)
+
+            *(mat+i+j) = rand()%10 + 1;
+}
+
+
+// liberating the space allocated to the matrice.
+
+void free_mat(int *mat)
+{
+
+    free(mat);
+
+}
+
+
+//prints out the binary form of an integer.
+void print_binary(int num) {
+
     int i,n,rm;
     i = num;
 
@@ -13,15 +68,15 @@ void print_binary(int num){
         printf("0");
 
     else if ( i == 1)
-            printf("1");
+        printf("1");
     else
     {
         for( n = 0; pow(2.0,n) <= i; n++)
-                        ;
+            ;
 
         n--;
 
-        while(n >= 0){
+        while(n >= 0) {
 
             if( i >= pow(2.0,n))
             {
@@ -43,22 +98,24 @@ void print_binary(int num){
 }
 
 
-int isPrime(int n) // if n prime returns value of n,else returns zero(0)
+//If argument is a prime it returns itself,otherwise returns 0.
+int isPrime(int n)
 {
-   int i, count = 0;
+    int i, count = 0;
 
-   for(i = 2; i < n; i++)
+    for(i = 2; i < n; i++)
 
-      if(n % i )
-        count += 1;
-   if(count == n - 2)
+        if(n % i )
+            count += 1;
+    if(count == n - 2)
         return (n);
-   else
-    return (0) ;
+    else
+        return (0) ;
 }
 
-int combo1(int p1,int p2) // checks out if the sum of two integers(primes preferable) minus 1 is a prime.
-{
+
+// checks out if the sum of two integers(primes preferable) minus 1 is a prime.
+int combo1(int p1,int p2){
 
     int sum= p1 + p2 - 1 ;
     int i;
@@ -69,6 +126,8 @@ int combo1(int p1,int p2) // checks out if the sum of two integers(primes prefer
 
 }
 
+
+//absolute value implementation.
 int bi_abs (int a)
 {
     if (a < 0)
@@ -76,24 +135,27 @@ int bi_abs (int a)
     return (a) ;
 }
 
+
+//prints in stdout the solutions of a quadratic equation.
 void  quadratic_equation(int a,int b,int c)
-  {
-          int discr = b*b - 4*a*c ;
-          
-          if(discr < 0)
-                  printf("No solutions availiable \n");
-          else
-                  if (discr == 0)
-                          printf(" One solution twice : %.2f \n",(float) (-b ) / (2*a));
-                  else
-                  {
-                          float m,n ;
-                          m = ((-b) + sqrt(discr)) / 2*a ;
-                          n = ((-b) - sqrt(discr)) / 2*a ;
-                          printf("Two solutions : %.2f , %.2f \n",m,n);
-                  }
-  }
-  
+{
+    int discr = b*b - 4*a*c ;
+
+    if(discr < 0)
+        printf("No solutions availiable \n");
+    else if (discr == 0)
+        printf(" One solution twice : %.2f \n",(float) (-b ) / (2*a));
+    else
+    {
+        float m,n ;
+        m = ((-b) + sqrt(discr)) / 2*a ;
+        n = ((-b) - sqrt(discr)) / 2*a ;
+        printf("Two solutions : %.2f , %.2f \n",m,n);
+    }
+}
+
+
+// absolute value implementation for doubles.
 double bi_abs_d (double a)
 {
     if (a < 0)
@@ -101,25 +163,30 @@ double bi_abs_d (double a)
     return (a) ;
 }
 
+
+// implemetation of "strlen" function.
 int bi_strlen(char *str)
 {
     int counter = 0;
-    for(counter = 0 ; str[counter]!='\0' ;counter ++)
+    for(counter = 0 ; str[counter]!='\0' ; counter ++)
         ;
-        return (counter) ;
+    return (counter) ;
 }
 
+
+// implementation of "strcmp" function.
 int bi_strcmp(char *str1,char *str2)
 {
     int i;
     if(bi_strlen(str1) != bi_strlen(str2))
         return (-1);
-    for(i = 0; i < bi_strlen(str1);i++)
+    for(i = 0; i < bi_strlen(str1); i++)
     {
         if(str1[i] != str2[i])
             return (-1);
     }
     return (0);
 }
+
 
 #endif // BIBLIOTECA_H_INCLUDED1
